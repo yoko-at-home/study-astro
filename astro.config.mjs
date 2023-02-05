b80b8e9adf959404704a5804df73760af9106c5b
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2023-02-05 22:55:43 +0900

    preact configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+// import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  integrations: [preact()],
+  // adapter: vercel({}),
+  adapter: netlify(),
+});
