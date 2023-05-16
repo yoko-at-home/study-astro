a1cc1062c0d5537de19d264efed23f902ac55440
yoko-at-home <tw107sf.ham@icloud.com>
2023-05-16 19:59:26 +0900

    update artist pictures

--- src/components/Artist.ts
+++ src/components/Artist.ts
@@ -1,32 +1,32 @@
 export const images = [
   {
-    href: "https://images.unsplash.com/photo-1471478331149-c72f17e33c73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80",
-    size: { width: 1300, height: 600 },
+    href: "https://zama-musicartist.info/concertreport/wp-content/uploads/S__113803279.jpg",
+    size: { width: 1300, height: 1000 },
     label: "yoko",
   },
   {
-    href: "https://images.unsplash.com/photo-1528143358888-6d3c7f67bd5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1041&q=80",
-    size: { width: 1300, height: 600 },
+    href: "https://zama-musicartist.info/concertreport/wp-content/uploads/S__113803282.jpg",
+    size: { width: 1300, height: 1000 },
     label: "piko",
   },
   {
-    href: "https://images.unsplash.com/photo-1631739714671-0de24b57d810?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1058&q=80",
+    href: "https://zama-musicartist.info/concertreport/wp-content/uploads/S__113803285.jpg",
     size: { width: 1300, height: 1000 },
     label: "toko",
   },
   {
-    href: "https://images.unsplash.com/photo-1471565661762-b9dfae862dbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-    size: { width: 1300, height: 600 },
+    href: "https://zama-musicartist.info/concertreport/wp-content/uploads/S__107962421.jpg",
+    size: { width: 1300, height: 1000 },
     label: "yoko",
   },
   {
-    href: "https://images.unsplash.com/photo-1454922915609-78549ad709bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80",
-    size: { width: 1300, height: 600 },
+    href: "https://zama-musicartist.info/concertreport/wp-content/uploads/S__105553933.jpg",
+    size: { width: 1300, height: 1000 },
     label: "piko",
   },
   {
-    href: "https://images.unsplash.com/photo-1597656370793-12900b6d9a28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
-    size: { width: 1300, height: 600 },
+    href: "https://zama-musicartist.info/concertreport/wp-content/uploads/S__104382473.jpg",
+    size: { width: 1300, height: 1000 },
     label: "toko",
   },
 ];
