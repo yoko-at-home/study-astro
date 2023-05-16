fed42f42446ec1af60d057df474a62afb0d91d67
yoko-at-home <tw107sf.ham@icloud.com>
2023-05-16 20:51:55 +0900

    add program component

--- src/consts.ts
+++ src/consts.ts
@@ -48,6 +48,21 @@ export const PERSONAL_INFO = {
     "Adobe製品と戯れる主婦 |  Nextjs・AstroでWebサイト制作. ホームページ管理人：座間市演奏家連盟 (WordPress Lolipop) ・ 女声合唱団ポレポーレ (NextJs microCMS Netlify). チラシ＆動画制作 (Illustrator, Premiere Pro, AE) | 元産業翻訳 | クラウドワークでトランスクリプト | 老人ホームへ慰問となにかと忙しい",
 } as const;
 
+export const EVENT_INFO = {
+  title: "Program",
+  subtitle: "xx",
+  one: "https://zama-musicartist.info/concertreport/wp-content/uploads/%E3%82%AF%E3%83%A9%E3%82%B7%E3%83%83%E3%82%AF%E3%83%95%E3%82%A7%E3%82%B9%E3%82%BF%EF%BC%92%EF%BC%90%EF%BC%92%EF%BC%92%E3%83%9A%E3%83%BC%E3%82%B81-1448x2048.jpg",
+  oneAlt: "aaa",
+  width: 300,
+  two: "https://zama-musicartist.info/concertreport/wp-content/uploads/%E3%82%AF%E3%83%A9%E3%82%B7%E3%83%83%E3%82%AF%E3%83%95%E3%82%A7%E3%82%B9%E3%82%BF%EF%BC%92%EF%BC%90%EF%BC%92%EF%BC%92%E3%83%9A%E3%83%BC%E3%82%B82-1448x2048.jpg",
+  twoAlt: "aaa",
+  three:
+    "https://zama-musicartist.info/concertreport/wp-content/uploads/%E3%82%AF%E3%83%A9%E3%82%B7%E3%83%83%E3%82%AF%E3%83%95%E3%82%A7%E3%82%B9%E3%82%BF%EF%BC%92%EF%BC%90%EF%BC%92%EF%BC%92%E3%83%9A%E3%83%BC%E3%82%B83-1448x2048.jpg",
+  threeAlt: "aaa",
+  four: "https://zama-musicartist.info/concertreport/wp-content/uploads/%E3%82%AF%E3%83%A9%E3%82%B7%E3%83%83%E3%82%AF%E3%83%95%E3%82%A7%E3%82%B9%E3%82%BF%EF%BC%92%EF%BC%90%EF%BC%92%EF%BC%92%E3%83%9A%E3%83%BC%E3%82%B84-1448x2048.jpg",
+  fourAlt: "aaa",
+} as const;
+
 export const SEO_INFO = [
   {
     name: "description",
